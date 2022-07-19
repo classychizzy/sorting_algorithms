7295da25b71164536b5501105c9d59e5e6c605de
classychizzy <chizobachukwuoma@gmail.com>
2022-07-19 16:40:14 +0000

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,11 +12,11 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - 1; j++)
 		{
-			if (array[j + 1] <  array[j])
+			if (array[j] >  array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
--- sort.h
+++ sort.h
@@ -1,5 +1,7 @@
 #ifndef __SORT_H__
 #define __SORT_H__
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
