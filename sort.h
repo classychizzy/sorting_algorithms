3b685cf96b66fb70c99351c22b1b75c96b220f8f
classychizzy <chizobachukwuoma@gmail.com>
2022-07-19 21:16:35 +0000

    task 9

--- sort.h
+++ sort.h
@@ -23,9 +23,11 @@ void print_list(const listint_t *list);
 /*sorting algorithms*/
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void quick_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
-
+void radix_sort(int *array, size_t size);
 #endif
