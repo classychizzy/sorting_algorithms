2a4acdd0e7cc73d4e16d08b5319ae0a263f2d0d7
classychizzy <chizobachukwuoma@gmail.com>
2022-07-19 14:55:22 +0000

    task 0

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "sort.h"
+/**
+ * insertion_sort_list- a function that sorts
+ * a doubly linked list using insertion
+ * @listint_t: the doubly linked list
+ * @list- pointer to the node
+ * Return: sorted doubly linked list
+ */
