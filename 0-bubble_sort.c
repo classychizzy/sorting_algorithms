eeb9872dd8066a44c2c61df4dff202c20e4c9324
classychizzy <chizobachukwuoma@gmail.com>
2022-07-19 15:10:04 +0000

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,10 +10,11 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
+	int flag, temp;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		flag = 0;
 		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -21,8 +22,13 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				flag = 1;
 				print_array(array, size);
 			}
+		}
+		if (flag == 0)
+		{
+			break;
 		}
 	}
 }
